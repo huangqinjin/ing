90274b859f6a284507ae2e65772f8bf834d48d12
huangqinjin <huangqinjin@gmail.com>
2021-09-22 20:14:11 +0800

    [spinlock] init

---
+++ include/ing/spinlock.hpp
@@ -0,0 +1,47 @@
+#ifndef ING_SPINLOCK_HPP
+#define ING_SPINLOCK_HPP
+
+#include <atomic>
+
+namespace ing
+{
+    class spinlock
+    {
+#if defined(__cpp_lib_atomic_value_initialization)
+        std::atomic_flag guard_;
+#else
+        std::atomic_flag guard_ = ATOMIC_FLAG_INIT;
+#endif
+
+    public:
+        spinlock() noexcept = default;
+        spinlock(const spinlock&) noexcept {}
+        spinlock& operator=(const spinlock&) noexcept { return *this; }
+
+        bool try_lock() noexcept
+        {
+            return !guard_.test_and_set(std::memory_order_acquire);
+        }
+
+        void unlock() noexcept
+        {
+            guard_.clear(std::memory_order_release);
+#if defined(__cpp_lib_atomic_wait)
+            guard_.notify_one();
+#endif
+        }
+
+        void lock() noexcept
+        {
+            while (!try_lock())
+            {
+#if defined(__cpp_lib_atomic_wait)
+                guard_.wait(true, std::memory_order_relaxed);
+#elif defined(__cpp_lib_atomic_flag_test)
+                while (guard_.test(std::memory_order_relaxed));
+#endif
+            }
+        }
+    };
+}
+#endif
