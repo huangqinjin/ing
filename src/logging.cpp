89757c46928c20bd33e2f72db7f55c2267f1b8c3
huangqinjin <huangqinjin@gmail.com>
2021-09-28 02:51:29 +0800

    [logging] format source location

--- src/logging.cpp
+++ src/logging.cpp
@@ -12,7 +12,9 @@
 #include <boost/log/expressions/message.hpp>
 #include <boost/log/expressions/formatters/stream.hpp>
 #include <boost/log/expressions/formatters/date_time.hpp>
+#include <boost/log/expressions/formatters/named_scope.hpp>
 #include <boost/log/expressions/formatters/auto_newline.hpp>
+#include <boost/log/expressions/formatters/wrap_formatter.hpp>
 
 #include <boost/log/support/date_time.hpp>
 #include <boost/log/utility/setup/console.hpp>
@@ -124,6 +126,39 @@ namespace ing::logging::expressions
     BOOST_LOG_ATTRIBUTE_KEYWORD(thread_name, ::ing::logging::expressions::names::thread_name(), ::ing::logging::attributes::current_thread_name::value_type)
     BOOST_LOG_ATTRIBUTE_KEYWORD(process_name, ::ing::logging::expressions::names::process_name(), ::ing::logging::attributes::current_process_name::value_type)
     BOOST_LOG_ATTRIBUTE_KEYWORD(scope, ::ing::logging::expressions::names::scope(), ::ing::logging::attributes::named_scope::value_type)
+
+
+    template<typename Keyword>
+    auto format_source_location(const Keyword& keyword, std::string_view format)
+    {
+        return boost::log::expressions::wrap_formatter(
+        [keyword, formatter = boost::log::expressions::aux::parse_named_scope_format(format.data(), format.data() + format.size())]
+        (boost::log::record_view const& rec, boost::log::formatting_ostream& strm)
+        {
+            if (const auto& loc = rec[keyword])
+            {
+                struct unbox
+                {
+                    boost::log::string_literal::const_iterator str;
+                    boost::log::string_literal::size_type len;
+
+                    void operator=(const char* s)
+                    {
+                        static_assert(sizeof(unbox) == sizeof(boost::log::string_literal));
+
+                        str = s;
+                        len = boost::log::string_literal::traits_type::length(s);
+                    }
+                };
+
+                using scope_entry = attributes::named_scope::scope_entry;
+                scope_entry entry({}, {}, loc->line(), scope_entry::function);
+                reinterpret_cast<unbox&>(entry.scope_name) = loc->function_name();
+                reinterpret_cast<unbox&>(entry.file_name) = loc->file_name();
+                formatter(strm, entry);
+            }
+        });
+    }
 }
 
 namespace ing
@@ -154,6 +189,7 @@ void ing::init_logging()
             << boost::log::expressions::format_date_time(logging::expressions::timestamp, "%H:%M:%S.%f") << ' '
             << '[' << logging::expressions::severity << ']' << ' '
             << '<' << logging::expressions::channel << '>' << ' '
+            << logging::expressions::format_source_location(logging::expressions::location, "%F(%l) '%n'") << ' '
             << '-' << ' '
             << boost::log::expressions::smessage
             << boost::log::expressions::auto_newline;
